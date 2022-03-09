5086013e80282a2a81c71640a0bfd7b1ce32a3d2
Cansiny0320 <1136843910@qq.com>
2022-03-09 15:57:55 +0800

    feat: 2059-hard-drop-string

---
+++ solutions/2059-hard-drop-string/solution.ts
@@ -0,0 +1,68 @@
+/*
+  2059 - Drop String
+  -------
+  by CaptainOfPhB (@CaptainOfPhB) #hard #template-literal #infer
+  
+  ### Question
+  
+  Drop the specified chars from a string.
+  
+  For example:
+  
+  ```ts
+  type Butterfly = DropString<'foobar!', 'fb'> // 'ooar!'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/2059
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Includes<
+  T extends string,
+  S extends string
+> = T extends `${infer H}${infer Rest}`
+  ? S extends H
+    ? true
+    : Includes<Rest, S>
+  : false
+
+type DropString<
+  S extends string,
+  R extends string
+> = S extends `${infer First}${infer Rest}`
+  ? Includes<R, First> extends true
+    ? DropString<Rest, R>
+    : `${First}${DropString<Rest, R>}`
+  : S
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<DropString<'butter fly!', ''>, 'butter fly!'>>,
+  Expect<Equal<DropString<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<'butter fly!', 'but'>, 'er fly!'>>,
+  Expect<
+    Equal<DropString<' b u t t e r f l y ! ', 'but'>, '     e r f l y ! '>
+  >,
+  Expect<Equal<DropString<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<
+    Equal<DropString<' b u t t e r f l y ! ', 'but'>, '     e r f l y ! '>
+  >,
+  Expect<
+    Equal<DropString<' b u t t e r f l y ! ', 'tub'>, '     e r f l y ! '>
+  >,
+  Expect<
+    Equal<DropString<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>
+  >,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2059/answer
+  > View solutions: https://tsch.js.org/2059/solutions
+  > More Challenges: https://tsch.js.org
+*/
