06d32df6ce253f5070e2add1a8658ee7aef76472
Cansiny0320 <1136843910@qq.com>
2022-03-13 20:30:01 +0800

    feat: 4425-medium-greater-than

---
+++ solutions/4425-medium-greater-than/solution.ts
@@ -0,0 +1,54 @@
+/*
+  4425 - Greater Than
+  -------
+  by ch3cknull (@ch3cknull) #medium #array
+  
+  ### Question
+  
+  In This Challenge, You should implement a type `GreaterThan<T, U>` like `T > U`
+  
+  Negative numbers do not need to be considered.
+  
+  For example
+  
+  ```ts
+  GreaterThan<2, 1> //should be true
+  GreaterThan<1, 1> //should be false
+  ```
+  
+  Good Luck!
+  
+  > View on GitHub: https://tsch.js.org/4425
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type GreaterThan<
+  Num1 extends number,
+  Num2 extends number,
+  CountArr extends unknown[] = []
+> = Num1 extends Num2
+  ? false
+  : CountArr['length'] extends Num2
+  ? true
+  : CountArr['length'] extends Num1
+  ? false
+  : GreaterThan<Num1, Num2, [...CountArr, unknown]>
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<GreaterThan<1, 0>, true>>,
+  Expect<Equal<GreaterThan<5, 4>, true>>,
+  Expect<Equal<GreaterThan<4, 5>, false>>,
+  Expect<Equal<GreaterThan<0, 0>, false>>,
+  Expect<Equal<GreaterThan<20, 20>, false>>
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4425/answer
+  > View solutions: https://tsch.js.org/4425/solutions
+  > More Challenges: https://tsch.js.org
+*/
